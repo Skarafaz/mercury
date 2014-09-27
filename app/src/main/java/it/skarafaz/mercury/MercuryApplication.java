5e113503dd6961dcd790617c3a7b85624c803bb4
Skarafaz <skarafaz@gmail.com>
2014-09-27 12:42:57 +0200

    dev: get sample config files from assets

--- app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
+++ app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
@@ -3,6 +3,8 @@ package it.skarafaz.mercury;
 import android.app.Application;
 import android.content.Context;
 
+import it.skarafaz.mercury.manager.ServerManager;
+
 /**
  * Created by Skarafaz on 20/09/2014.
  */
@@ -13,6 +15,7 @@ public class MercuryApplication  extends Application{
     public void onCreate() {
         super.onCreate();
         context = this;
+        ServerManager.getInstance().init();
     }
 
     public static Context getContext() {
--- app/src/main/java/it/skarafaz/mercury/manager/ServerManager.java
+++ app/src/main/java/it/skarafaz/mercury/manager/ServerManager.java
@@ -1,7 +1,18 @@
 package it.skarafaz.mercury.manager;
 
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import it.skarafaz.mercury.MercuryApplication;
 import it.skarafaz.mercury.data.Server;
 
 /**
@@ -22,6 +33,31 @@ public class ServerManager {
     }
 
     public void init() {
+        List<String> files = getSampleConfigFiles();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            for (String file: files) {
+                servers.add(mapper.readValue(file, Server.class));
+            }
+        } catch (IOException e) {
+            Log.e(ServerManager.class.getSimpleName(), e.getMessage());
+        }
+    }
+
+    private List<String> getSampleConfigFiles() {
+        List<String> files = new ArrayList<String>();
+        AssetManager assetManager = MercuryApplication.getContext().getAssets();
+        try {
+            String[] assets = assetManager.list("");
+            for (String asset: assets) {
+                InputStream inputStream = assetManager.open(asset);
+                files.add(IOUtils.toString(inputStream, "UTF-8"));
+                inputStream.close();
+            }
+        } catch (IOException e) {
+            Log.e(ServerManager.class.getSimpleName(), e.getMessage());
+        }
+        return files;
     }
 
     public List<Server> getServers() {
