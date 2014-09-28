44da8abd0717cd4d1d740a55b73b540c7d6eeeba
Skarafaz <skarafaz@gmail.com>
2014-09-28 15:32:33 +0200

    dev: scrollable tabs

--- app/src/main/java/it/skarafaz/mercury/adapter/ServerPagerAdapter.java
+++ app/src/main/java/it/skarafaz/mercury/adapter/ServerPagerAdapter.java
@@ -1,8 +1,7 @@
 package it.skarafaz.mercury.adapter;
 
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import java.util.List;
@@ -26,5 +25,10 @@ public class ServerPagerAdapter extends FragmentStatePagerAdapter {
     @Override
     public int getCount() {
         return servers.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return servers.get(position).getName();
     }
 }
