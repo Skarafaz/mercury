f06ff43dca6de9b7dec6edf62b9aa7999e6a9a1f
Skarafaz <skarafaz@gmail.com>
2014-09-28 18:28:39 +0200

    clean-up

--- app/src/main/java/it/skarafaz/mercury/activity/MainActivity.java
+++ app/src/main/java/it/skarafaz/mercury/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package it.skarafaz.mercury.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
--- app/src/main/java/it/skarafaz/mercury/fragment/ServerFragment.java
+++ app/src/main/java/it/skarafaz/mercury/fragment/ServerFragment.java
@@ -1,7 +1,6 @@
 package it.skarafaz.mercury.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
