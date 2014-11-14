fb1bca86a1d759a8aa61e45097530a89fb66cb0c
Skarafaz <skarafaz@gmail.com>
2014-11-15 00:09:57 +0100

    clean-up: imports

--- app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
+++ app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
@@ -3,8 +3,6 @@ package it.skarafaz.mercury;
 import android.app.Application;
 import android.content.Context;
 
-import it.skarafaz.mercury.manager.ServerManager;
-
 public class MercuryApplication  extends Application{
     private static Context context;
 
--- app/src/main/java/it/skarafaz/mercury/fragment/ServerFragment.java
+++ app/src/main/java/it/skarafaz/mercury/fragment/ServerFragment.java
@@ -1,14 +1,10 @@
 package it.skarafaz.mercury.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import it.skarafaz.mercury.R;
 import it.skarafaz.mercury.adapter.CommandListAdapter;
