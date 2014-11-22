b6e3bff2b237db7444e9d255c13d056f1824b428
Skarafaz <skarafaz@gmail.com>
2014-11-22 11:34:33 +0100

    dev: config dir prefs entry

--- app/src/main/java/it/skarafaz/mercury/activity/PreferencesActivity.java
+++ app/src/main/java/it/skarafaz/mercury/activity/PreferencesActivity.java
@@ -2,8 +2,13 @@ package it.skarafaz.mercury.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import it.skarafaz.mercury.R;
+import it.skarafaz.mercury.manager.PreferencesManager;
 
 public class PreferencesActivity extends Activity {
 
@@ -11,5 +16,14 @@ public class PreferencesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        TextView configDirValue = (TextView) findViewById(R.id.config_dir_value);
+        configDirValue.setText(String.format("/%s", PreferencesManager.getInstance().getConfigDir()));
+        LinearLayout configDir = (LinearLayout) findViewById(R.id.config_dir);
+        configDir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(PreferencesActivity.class.getSimpleName(), "configDir");
+            }
+        });
     }
 }
