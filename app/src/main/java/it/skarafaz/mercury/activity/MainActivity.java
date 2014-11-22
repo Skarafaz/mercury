576429b03272f2d8eee11daee0c644df28d923fe
Skarafaz <skarafaz@gmail.com>
2014-11-22 10:59:50 +0100

    dev: added prefs activity

--- app/src/main/java/it/skarafaz/mercury/activity/MainActivity.java
+++ app/src/main/java/it/skarafaz/mercury/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package it.skarafaz.mercury.activity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -70,18 +71,16 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, PreferencesActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/it/skarafaz/mercury/activity/PreferencesActivity.java
@@ -0,0 +1,15 @@
+package it.skarafaz.mercury.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import it.skarafaz.mercury.R;
+
+public class PreferencesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
