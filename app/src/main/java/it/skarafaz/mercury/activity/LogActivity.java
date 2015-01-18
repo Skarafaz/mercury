e6263b62194ad569b0dd1abc97350f055a02c5d5
Skarafaz <skarafaz@gmail.com>
2015-01-18 14:45:34 +0100

    dev: log activity

--- app/src/main/java/it/skarafaz/mercury/activity/LogActivity.java
+++ app/src/main/java/it/skarafaz/mercury/activity/LogActivity.java
@@ -1,13 +1,22 @@
 package it.skarafaz.mercury.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.skarafaz.mercury.R;
 
@@ -17,9 +26,11 @@ public class LogActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setElevation(0);
         setContentView(R.layout.activity_log);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(new ArrayAdapter<>(this, R.layout.log_list_item, readLogFile()));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,6 +48,17 @@ public class LogActivity extends ActionBarActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private List<String> readLogFile() {
+        File file = new File(getDir("log", Context.MODE_PRIVATE), "mercury.log");
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = FileUtils.readLines(file, "UTF-8");
+        } catch (IOException e) {
+            logger.debug(e.getMessage());
+        }
+        return lines;
     }
 
     private void clear() {
