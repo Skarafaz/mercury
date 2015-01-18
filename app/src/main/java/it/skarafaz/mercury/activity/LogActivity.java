a114f66c10d6e28c19fd278328a2716824410f7d
Skarafaz <skarafaz@gmail.com>
2015-01-18 15:53:54 +0100

    refactoring

--- app/src/main/java/it/skarafaz/mercury/activity/LogActivity.java
+++ app/src/main/java/it/skarafaz/mercury/activity/LogActivity.java
@@ -1,79 +1,15 @@
 package it.skarafaz.mercury.activity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import it.skarafaz.mercury.R;
 
 public class LogActivity extends ActionBarActivity {
-    public static final String LOG_DIR = "log";
-    public static final String LOG_FILE = "mercury.log";
-    public static final String ENCODING = "UTF-8";
-    private static final Logger logger = LoggerFactory.getLogger(LogActivity.class);
-    ListView list;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setElevation(0);
         setContentView(R.layout.activity_log);
-        list = (ListView) findViewById(R.id.list);
-        reload();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_log, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.clear:
-                clearLog();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void reload() {
-        list.setAdapter(new ArrayAdapter<>(this, R.layout.log_list_item, readLog()));
-    }
-
-    private List<String> readLog() {
-        File file = new File(getDir(LOG_DIR, Context.MODE_PRIVATE), LOG_FILE);
-        List<String> lines = new ArrayList<>();
-        try {
-            lines = FileUtils.readLines(file, ENCODING);
-        } catch (IOException e) {
-            logger.debug(e.getMessage());
-        }
-        return lines;
-    }
-
-    private void clearLog() {
-        File logDir = getDir(LOG_DIR, Context.MODE_PRIVATE);
-        for (File file : logDir.listFiles()) {
-            file.delete();
-        }
-        reload();
     }
 }
