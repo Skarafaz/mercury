3d50734cd6e4eeae730ca406d57a12dcb8d0b051
Skarafaz <skarafaz@gmail.com>
2015-01-31 11:16:56 +0100

    dev: custom tab strip

---
+++ app/src/main/java/it/skarafaz/mercury/widget/PagerTabStrip.java
@@ -0,0 +1,26 @@
+package it.skarafaz.mercury.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+public class PagerTabStrip extends android.support.v4.view.PagerTabStrip {
+    private static final int PADDING_BOTTOM = 9; // dp
+
+    public PagerTabStrip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        for (int i = 0; i < getChildCount(); i++) {
+            View v = getChildAt(i);
+            if (v instanceof TextView) {
+                TextView t = (TextView) v;
+                t.setPadding(0, 0, 0, getPixelsFromDps(PADDING_BOTTOM));
+            }
+        }
+    }
+
+    private int getPixelsFromDps(int dps) {
+        final float density = getResources().getDisplayMetrics().density;
+        return (int) (dps * density + 0.5f);
+    }
+}
