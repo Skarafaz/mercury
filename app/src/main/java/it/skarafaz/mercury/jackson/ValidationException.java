197b8347afe1e2d8d76d9cbe12622a433967920a
skarafaz <skarafaz@gmail.com>
2016-09-04 13:37:35 +0200

    refactoring

--- app/src/main/java/it/skarafaz/mercury/jackson/ValidationException.java
+++ app/src/main/java/it/skarafaz/mercury/jackson/ValidationException.java
@@ -3,7 +3,7 @@ package it.skarafaz.mercury.jackson;
 public class ValidationException extends Exception {
     private static final long serialVersionUID = 5137298023298850552L;
 
-    public ValidationException(String detailMessage) {
-        super(detailMessage);
+    public ValidationException(String message) {
+        super(message);
     }
 }
--- app/src/main/java/it/skarafaz/mercury/manager/ConfigManager.java
+++ app/src/main/java/it/skarafaz/mercury/manager/ConfigManager.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,13 +25,15 @@ import it.skarafaz.mercury.model.Server;
 
 public class ConfigManager {
     private static final Logger logger = LoggerFactory.getLogger(ConfigManager.class);
+    private static final String CONFIG_DIR = "Mercury-SSH";
+    public static final String JSON_EXT = "json";
     private static ConfigManager instance;
     private File configDir;
     private ServerMapper mapper;
     private List<Server> servers;
 
     private ConfigManager() {
-        configDir = new File(Environment.getExternalStorageDirectory(), "Mercury-SSH");
+        configDir = new File(Environment.getExternalStorageDirectory(), CONFIG_DIR);
         mapper = new ServerMapper();
         servers = new ArrayList<>();
     }
@@ -56,7 +59,7 @@ public class ConfigManager {
         if (isExternalStorageReadable()) {
             if (storagePermissionGranted()) {
                 if (configDir.exists() && configDir.isDirectory()) {
-                    for (File file : FileUtils.listFiles(configDir, new String[]{"json", "JSON"}, false)) {
+                    for (File file : listConfigFiles()) {
                         try {
                             servers.add(mapper.readValue(file));
                         } catch (IOException | ValidationException e) {
@@ -77,6 +80,10 @@ public class ConfigManager {
             result = LoadConfigExitStatus.CANNOT_READ_EXT_STORAGE;
         }
         return result;
+    }
+
+    private Collection<File> listConfigFiles() {
+        return FileUtils.listFiles(configDir, new String[] { JSON_EXT, JSON_EXT.toUpperCase() }, false);
     }
 
     private boolean storagePermissionGranted() {
