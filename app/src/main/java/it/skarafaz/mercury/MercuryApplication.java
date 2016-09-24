63998e18a9d84c1c190a9f91273efeb1a673c549
skarafaz <skarafaz@gmail.com>
2016-09-24 11:51:16 +0200

    dev: event bus subscriber index

--- app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
+++ app/src/main/java/it/skarafaz/mercury/MercuryApplication.java
@@ -8,6 +8,7 @@ import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.view.ViewConfiguration;
 
+import org.greenrobot.eventbus.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +28,8 @@ public class MercuryApplication extends Application {
         super.onCreate();
 
         context = this;
+
+        EventBus.builder().addIndex(new EventBusIndex()).build();
 
         // hack for devices with hw options button
         try {
