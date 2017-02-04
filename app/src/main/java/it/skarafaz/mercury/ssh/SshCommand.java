8f174e3806ddbd740332247a23c8cd397ed2d3d7
skarafaz <skarafaz@gmail.com>
2017-02-04 14:33:05 +0100

    dev: copyright header

--- app/src/main/java/it/skarafaz/mercury/ssh/SshCommand.java
+++ app/src/main/java/it/skarafaz/mercury/ssh/SshCommand.java
@@ -1,3 +1,23 @@
+/*
+ * Mercury-SSH
+ * Copyright (C) 2017 Skarafaz
+ *
+ * This file is part of Mercury-SSH.
+ *
+ * Mercury-SSH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mercury-SSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mercury-SSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.skarafaz.mercury.ssh;
 
 import com.jcraft.jsch.ChannelExec;
